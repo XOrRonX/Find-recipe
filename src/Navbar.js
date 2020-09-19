983f63711d0142c3a8c5af048435d09e05f1564e
Or Ron <or.ron7@gmail.com>
2020-09-19 12:19:48 +0300

    s

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,12 +6,12 @@ class Navbar extends Component {
     return (
       <div className="main">
         <div className="jumbotron">
-          <h1 className="display-4">Welcome to Recipes!</h1>
+          <h1 className="display-4">Find your next meal!</h1>
           <p className="lead">
-            This is a useful website for discover new recipes.
+            A useful website for discover new recipes.
           </p>
           <hr className="my-4" />
-          <p>Please insert down below kind of food you woud like to make.</p>
+          <p>So what kind of food woud you like to make?</p>
           <p className="lead">
             <a className="btn btn-outline-dark" href="https://developer.edamam.com/edamam-docs-recipe-api" role="button">
               Learn more
